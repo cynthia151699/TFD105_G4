78c163c1799033b3ae5b462ef9dadef1156b7884
wupeiru122 <qwejkk12372@gmail.com>
2022-03-25 17:03:35 +0800

    套件載入成功

--- src/js/main.js
+++ src/js/main.js
@@ -47,3 +47,17 @@ $("a.ham_link").on("click", function(e){
     e.preventDefault();
     $("ol.sub_ham").toggle("-on");
 });
+
+
+$(document).ready(function() {
+    $('#myContainer').multiscroll({
+        sectionsColor: ['#ff5f45', '#0798ec', '#fc6c7c'],
+        anchors: ['first', 'second', 'third'],
+        menu: '#menu',
+            navigation: true,
+            navigationTooltips: ['One', 'Two', 'Three'],
+            loopBottom: true,
+            loopTop: true
+    });
+});
+
