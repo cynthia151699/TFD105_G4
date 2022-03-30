20b0a442adca9bb30b494d8b3d7b73d2ce93fc0a
cy4309 <cy4309@gmail.com>
2022-03-30 21:05:56 +0800

    game-01-fix.icons

--- src/js/game.js
+++ src/js/game.js
@@ -2,3 +2,20 @@
 
 // <!-- Initialize Swiper -->
 var swiper = new Swiper(".mySwiper", {});
+
+// 變數宣告
+var g1 = document.getElementById('g1');
+var g2 = document.getElementById('g2');
+var g3 = document.getElementById('g3');
+var g4 = document.getElementById('g4');
+var g5 = document.getElementById('g5');
+var g1_1 = document.getElementById('g1_1');
+var g2_1 = document.getElementById('g2_1');
+var g3_1 = document.getElementById('g3_1');
+var g4_1 = document.getElementById('g4_1');
+var g5_1 = document.getElementById('g5_1');
+
+//抓到img陣列，準備跑迴圈做配對
+let ccc = document.getElementsByTagName("img");
+// console.log(ccc);
+
