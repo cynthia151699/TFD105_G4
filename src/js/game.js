8bc23dfbba300d716aa057a6a412884535e4579b
cy4309 <cy4309@gmail.com>
2022-04-01 17:11:23 +0800

    game-01-add.activeclass

--- src/js/game.js
+++ src/js/game.js
@@ -15,16 +15,79 @@ var g3_1 = document.getElementById('g3_1');
 var g4_1 = document.getElementById('g4_1');
 var g5_1 = document.getElementById('g5_1');
 
-//抓到img陣列，準備跑迴圈做配對
-let ccc = document.getElementsByTagName("img");
-// console.log(ccc[5]);
-for (let i = 0; i < ccc.length; i++) {
-    if (i == 1 && 6) {
-        alert("congrats!");
-    }else{
+var ddd = document.querySelectorAll("img");
+// console.log(ddd);
+// console.log(ddd.length);
 
-    }
-};
+// ddd.forEach(function(value,index,arr){
+//     console.log(index);
+// });
+
+// document.addEventListener("drag", function(){
+//     if (ddd[0] && ddd[5] == )
+// });
+
+// console.log(document.getElementsByClassName("swiper-slide-active")[1]);
+let active1 = document.getElementsByClassName("swiper-slide-active")[0];
+// console.log(active1);
+let active2 = document.getElementsByClassName("swiper-slide-active")[1];
+
+
+// if active1 active2 在這兩個class的時候做動作
+document.addEventListener("click", function(){
+    if (active1);
+});
+
+
+// switch
+// case 1
+// break
+
+// else
+// congrats
+
+// active1.addEventListener("click", function(){
+//     if ( active2.ddd[0] - active1.ddd[5] == 5){
+//         console.log(active2.ddd.length);
+//         console.log(active1.ddd.length);
+//         alert(congrats);
+//     } else {
+//         alert("continue");
+//     }
+// });
+
+// if ("g1" == "g1_1")
+// actvie1#g1 == active#g1_1
+
+// ddd[0]-ddd[9]
+
+// ddd[
+//     {
+//         img0,
+//         img1,
+//         img2,
+//         img3,
+//     },
+// ]
+
+//抓到img陣列，準備跑迴圈做配對
+// let ccc = document.getElementsByTagName("img");
+// console.log(ccc[5]);
+// document.addEventListener("click", function(e){
+
+    // for (let i = 0; i < ccc.length; i++) {
+        // console.log(i);
+        // if (i == 1 && i == 6) {
+            // alert("congrats!");
+        // }else{}
+
+        // var bbb = ccc[0].getAttribute("title");
+        // if (title_text != null) {
+        //     alert(title_text);
+        // }
+    // };
+
+// });
 
 // transform:translate3d (0px, 0px, 0px); //0 5
 // transform:translate3d (-755px, 0px, 0px); //1 6
