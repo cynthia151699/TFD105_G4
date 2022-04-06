50944b69693e130b8cd187d31d53c45647ccc446
cy4309 <cy4309@gmail.com>
2022-04-06 15:20:16 +0800

    game-01-fix.iconSize

--- src/js/game.js
+++ src/js/game.js
@@ -2,11 +2,11 @@
 
 // <!-- Initialize Swiper -->
 let upperImgId = ""; // 紀錄上邊圖片ID
-let lowerImgId = "";// 紀錄下邊圖片ID
+let lowerImgId = ""; // 紀錄下邊圖片ID
 const swiper = new Swiper('.mySwiper', {
     loop: true,
-    // slidesPerGroup: 1,
-    // speed: 700,
+    // slidesPerGroup: 4,
+    // speed: 500,
     on: {
         init: function () {
             // console.log('swiper initialized');
@@ -38,12 +38,34 @@ const swiper = new Swiper('.mySwiper', {
 });
 
 // let disappear = document.querySelectorAll(".swiper");
+
 // let disappear1 = document.getElementsByClassName("swiper")[0];
 // disappear1.style = "display:none";
 // let disappear2 = document.getElementsByClassName("swiper")[1];
 // disappear2.style = "display:none;";
 // let appear = document.getElementsByClassName("congrats")[0];
 // appear.style = "display:flex";
+
+// let rotation = document.getElementsByClassName('rotate')[0];
+// console.log(rotation);
+
+// rotation.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const swiper = new Swiper(".mySwiper", {
+//         loop: true,
+//         slidesPerGroup: 1,
+//         speed: 5000,
+
+//     });
+// });
+
+//亂數
+// let rotation = document.getElementsByClassName("rotate")[0];
+// console.log(rotation);
+// rotation.addEventListener("click", function(array){
+    // array.sort(() => Math.random() - 0.5);
+// });
+
 
 
 // const swiper = new Swiper(".mySwiper", {
@@ -52,15 +74,6 @@ const swiper = new Swiper('.mySwiper', {
 //     speed: 700,
 // });
 
-// let rotation = document.getElementsByClassName('rotate')[0];
-// // console.log(rotation);
-
-// rotation.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const swiper = new Swiper(".mySwiper", {
-//         loop: true,
-//         slidesPerGroup: 1,
-//         speed: 50000,
-
-//     });
-// });
+// 1. 速度問題
+// 2. 陣列改css
+// 3. 亂數的作法
