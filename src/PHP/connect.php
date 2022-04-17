f327fc6c23fe2876cd2b79a57362e0cd7436144f
Pastanks <gn02212656@gmail.com>
2022-04-17 15:39:47 +0800

    php

---
+++ src/PHP/connect.php
@@ -0,0 +1,30 @@
+<?php
+
+       //MySQL相關資訊
+       $db_host = "127.0.0.1";
+       $db_user = "root";
+       $db_pass = "password";
+       $db_select = "G4";
+
+       //建立資料庫連線物件
+       $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
+
+       //建立PDO物件，並放入指定的相關資料
+       $pdo = new PDO($dsn, $db_user, $db_pass);
+
+       //---------------------------------------------------
+       $ACCOUNT = $_POST["ACCOUNT"];
+       $PASSWORD = $_POST["PASSWORD"];
+       $NAME = $_POST["NAME"];
+       $PHONE = $_POST["PHONE"];
+       
+
+
+
+
+
+
+
+
+
+?>
--- src/php/member.php
+++ src/php/member.php
@@ -1,5 +1,34 @@
 <?php
 
-echo "member page php";
+       include('connect.php');
+
+       //新增
+       $sql = "INSERT INTO MEMBER(  ACCOUNT, PASSWORD, NAME, PHONE, DATE) VALUES
+       ('$ACCOUNT', '$PASSWORD', '$NAME', '$PHONE', NOW())";
+
+       //執行
+       $pdo->exec($sql);
+
+       echo "新增成功!"
+    //    header("Location: Select.php");
+
+
+    // 登入
+    $sql =  "select * from member where ACCOUNT = '$account' and PASSWORD = '$password' ";
+
+    $statement = $pdo->query($sql);
+    $data = $statement->fetchAll(); 
+
+    if(count($data) > 0){
+    //轉址
+    session_start();
+    $_SESSION["memberID"] = "$account";
+    echo '登入成功';
+    // 再用登入成功傳到前面去判定
+    header("Location:front_member_info.html");
+    }else{
+    echo "帳號或密碼錯誤";
+    header("Location:front_member.html");
+    }
 
 ?>
