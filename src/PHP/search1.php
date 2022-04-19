8accb013afef1cd945034ef2af166c2d640fd0d5
Pastanks <gn02212656@gmail.com>
2022-04-19 10:37:44 +0800

    MEMBER_40_PHP

--- src/PHP/search1.php
+++ src/PHP/search1.php
@@ -4,7 +4,7 @@
        $db_host = "127.0.0.1";
        $db_user = "root";
        $db_pass = "password";
-       $db_select = "G4";
+       $db_select = "g4";
 
        //建立資料庫連線物件
        $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
@@ -13,17 +13,18 @@
        $pdo = new PDO($dsn, $db_user, $db_pass);
 
        //---------------------------------------------------
-       $ACCOUNT = $_POST["ACCOUNT"];
-       $PASSWORD = $_POST["PASSWORD"];
-       $NAME = $_POST["NAME"];
-       $PHONE = $_POST["PHONE"];
-       
+   
+    
+       session_start();
+ 
+        $mail = $_SESSION["memberID"];
+        $sql =  "select * from member where mail = '$mail'";
+        $statement = $pdo->query($sql);
+        
+        $data = $statement->fetchAll();
 
-
-
-
-
-
+        $data_json_en = json_encode($data);
+        echo $data_json_en;
 
 
 
