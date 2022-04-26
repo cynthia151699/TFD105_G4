319c888df6c828d1b727ed6c05685c4764ee1b22
cynthia151699 <cynthia151699@gmail.com>
2022-04-26 12:58:13 +0800

    [style] js beautifier

---
+++ src/js/cart_info.js
@@ -0,0 +1,39 @@
+//session抓到的價錢，丟進渲染
+document.getElementsByClassName("piece_info")[0].innerHTML =
+  sessionStorage.getItem("piece1"); //1.數量1從sessionStorage抓
+document.getElementsByClassName("piece_info")[1].innerHTML =
+  sessionStorage.getItem("piece2"); //2.數量2從sessionStorage抓
+document.getElementsByClassName("smallPrice")[0].value =
+  sessionStorage.getItem("smallPrice1"); //3.小計1從sessionStorage抓
+document.getElementsByClassName("smallPrice")[1].value =
+  sessionStorage.getItem("smallPrice2"); //4.小計2從sessionStorage抓
+document.getElementById("totalCount").innerHTML =
+  sessionStorage.getItem("totalCount"); //5.總數從sessionStorage抓
+document.getElementById("totalPrice").innerHTML =
+  sessionStorage.getItem("totalPrice"); //6.總價從sessionStorage抓
+document.getElementById("couponUse").innerHTML =
+  sessionStorage.getItem("couponUse"); //7.coupon使用狀態從sessionStorage抓
+//點擊送出按鈕會轉址
+document.getElementById("orderSend").value =
+  sessionStorage.getItem("totalPrice");
+
+  document.getElementById("info").addEventListener("click", function (event) {
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "./PHP/Memberticket.php",
+      dataType: "json",
+      async: false,
+      success: function (data) {
+        let Namevalue = data[0].NAME;
+        let Namecode = document.getElementById("Name");
+        Namecode.value = Namevalue;
+        let Phonevalue = data[0].PHONE;
+        let Phonecode = document.getElementById("Phone");
+        Phonecode.value = Phonevalue;
+        let emailvalue = data[0].mail;
+        let emailcode = document.getElementById("Email");
+        emailcode.value = emailvalue;
+      },
+    });
+  });
--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,18 @@
+var myFullpage = new fullpage("#fullpage", {
+  sectionsColor: ["#293241", "#293241"],
+  lazyLoad: true,
+});
+
+$("#btn_scroll").on("click", function () {
+  console.log("ttt");
+  $("#htmlScroll").animate(
+    {
+      scrollTop: $(".currExhibition").offset().top,
+    },
+    1000
+  );
+});
+
 document.documentElement.className = "js";
 var supportsCssVars = function () {
   var e,
