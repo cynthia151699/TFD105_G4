159be5dbb7eea018b319520630031eac0b8a4a57
elena1001 <elena971001@gmail.com>
2022-04-27 09:55:12 +0800

    front_news_19_vue更動&charming檔案刪除

--- src/js/front_news.js
+++ src/js/front_news.js
@@ -1,4 +1,6 @@
-
+document.documentElement.className = "js";
+var supportsCssVars = function () { var e, t = document.createElement("style"); return t.innerHTML = "root: { --tmp-var: bold; }", document.head.appendChild(t), e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")), t.parentNode.removeChild(t), e };
+supportsCssVars() || alert("Please view this demo in a modern browser that supports CSS Variables.");
 
 new Vue({
     el: '#news_wrapper',
@@ -835,6 +837,3 @@ new Vue({
 });
 
 
-document.documentElement.className = "js";
-var supportsCssVars = function () { var e, t = document.createElement("style"); return t.innerHTML = "root: { --tmp-var: bold; }", document.head.appendChild(t), e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")), t.parentNode.removeChild(t), e };
-supportsCssVars() || alert("Please view this demo in a modern browser that supports CSS Variables.");
